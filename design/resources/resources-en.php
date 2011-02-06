36e8a40a9f4793c1129833b4caccc936b8c91209
Morgan Cugerone <morgaan@gmail.com>
2011-02-06 11:01:42 +0000

    project sources commit

---
+++ design/resources/resources-en.php
@@ -0,0 +1,151 @@
+<?
+
+$language="Language";
+$username="Username";
+$password="Password";
+$password2="Password again";
+$pwdQuestion="Question in case of password loss";
+$pwdAnswer="Answer";
+$lostPassword="Lost Password?";
+
+$messageFolioNotInit="Your mnlfolio admin account is not initialized yet.<br/>You must define a username and a password<br/>in order to be able to manage your mnlfolio. <br/>To do so, please fill the below form :";
+$messagePasswordsMismatch="Error : 'Password' and 'Password again' do not match";
+$messageWrongPassword="Error : Wrong password, please try again";
+$messagePasswordChanged="Your password has been updated";
+$messagePwdAnswerMismatch="Error : The answer is incorrect!";
+
+$messageInstallComplete="Installation complete";
+$messageInstallCompleteDescription="Your mnlfolio is now successfully installed. Use the above tabs to start configuring it :)<br/> <b>If you don't see many tabs hit the refresh button on your web browser.</b>";
+
+$titleStep="Step";
+$messageInstallStep1Hello="Hello";
+$messageInstallStep1FolioNotRegistered="Your mnlfolio is not registered to Flickr yet!";
+$messageInstallStep1NeedCompletion="You need to complete a few steps...";
+$messageInstallStep1InstructionsPart1="Register your folio as an application to Flickr so it will be able to access your Flickr photo sets.<br/><br/>To do so, you will need to follow the link below ('--> Register your application to Flickr <--') where you'll be asked about this folio.<br/><br/><b>Before clicking the link, please read carefully the steps you will need to complete :</b><br/><br/>1) Inform Flickr of the nature of your folio while clicking on the key appliance button associated to the aim of your folio (Commercial | Non Commercial). The choice of the nature belongs to you.<br/>2) Fill and submit the application description form. The choice of the answers belongs to you by Flickr.<br/>3) Copy out to the below form the 'Key' and 'Secret' that are provided to you. These lasts identify your folio to Flickr.<br/>4) Leave the Flickr window open, you will need it in the Step 2.<br/>5) Finally, click on 'Next'.<br/><br/>&nbsp;&nbsp;&nbsp;";
+$messageInstallStep1InstructionsPart2="--> Register your application to Flickr <--</a> (The page will open in a new window/tab).<br/>";
+$messageInstallStep1Key="Key";
+$messageInstallStep1Secret="Secret";
+
+$messageInstallStep2InstructionsPart1="Now you need to specify a 'Callback URL'. To do so copy the below link (in red) : ";
+$messageInstallStep2InstructionsPart2="On the flickr page you left open, you must find below the 'Secret' a link called 'Edit auth flow for this app'.<br/>If you follow this link you will be able to fill a 'Callback URL' field by pasting the link you have just copied. Once paste made, hit the 'Save changes' button.";
+$messageInstallStep2InstructionsPart3="Click the link below in order to authorize your folio to access your flickr account. Once you hit the 'authorize it' button you'll be brought back to this current page.";
+$messageInstallStep2InstructionsPart4="I have set my Callback URL so I can now Authorize my folio;"
+
+$messageCouldntDelete="Couldn't delete";
+$messageCleanCacheSucceed="Clean cache succeed";
+
+$messageUploadLayout="Upload a layout";
+$messageUploadLogo="Upload a logo";
+$messageDeleteLayout="Delete a layout";
+$messageDeleteLogo="Delete a logo";
+$layout="layout";
+$logo="logo";
+
+$tabLabelMore="+";
+$tabLabelConfiguration="Configuration";
+$tabLabelAppearance="Appearance";
+$tabLayouts="Layouts";
+$tabLogos="Logos";
+$tabLabelMySets="My Sets";
+$tabLabelSeeFolio="See My Folio";
+
+$unselectedSets="Unselected set(s)";
+$selectedSets="Selected set(s)";
+$defaultSet="(by default)";
+
+$appearenceParams="Parameters";
+$appearenceParamsBody="Body";
+$appearenceParamsLinks="Links";
+$appearenceParamsContact="Contact";
+$appearenceParamsCopyright="Copyright";
+$appearenceParamsPhotoTitle="Photo title";
+$appearenceParamsPhotoDescription="Photo description";
+$appearenceParamsPhotoThumbFrame="Thumb frame";
+$appearenceParamsPhotoThumb="Thumb";
+$appearenceParamsSelectedPhotoThumbFrame="Selected photo thumb frame";
+$appearenceParamsSelectedPhotoThumb="Selected photo thumb";
+$appearenceParamsPhotoThumbMouseOver="Thumb (mouse over)";
+$appearenceParamsSelectedPhotoFrame="Selected Photo frame";
+$appearenceParamsSelectedPhoto="Selected Photo";
+$appearencePreview="Preview";
+$appearenceBodyText="Body Text";
+$appearenceLinks="Links";
+$appearenceContact="Contact";
+$appearenceCopyright="(c) copyright";
+$appearencePhotoTitle="Photo Title";
+
+$btnChooseFile="Choose a file";
+$btnUpload="Upload";
+$btnSave="Save";
+$btnConnect="Connect";
+$btnSaveChanges="Save Changes";
+$btnSetAsDefault="Set As Default";
+$btnClearDefault="Clear Default";
+$btnResetCache="Reset Cache";
+$btnSignOut="Sign Out";
+$btnNext="Next";
+$btnRenewPwd="Reset password";
+$btnDelete="Delete";
+$btnResetAll="Reset all";
+$btnResetFlickrLink="Reset flickr link";
+
+$configLanguage="Language";
+$configPageTitle="Page Title";
+$configShowPhotoDescription="Show photo description";
+$configShowPhotoTitle="Show Photo Title";
+$configPhotoSize="Selected photo size";
+$configGUIWidth="Viewer width in pixels (without thumbnails panel)";
+$configGUIHeight="Viewer height in pixels (without thumbnails panel)";
+$configColumns="Number of thumbs by columns";
+$configRows="Number of thumbs by rows";
+$configLayout="Viewer layout";
+$configEnableKeyboardControl="Enable keyboard control";
+$configEnableScrollWheelControl="Enable scroll wheel control";
+$configTweakSaveImageAs="Tweak Save Image As... option to prevent downloading your photos";
+$configPreviousPhotoControlLabel="Label for previous photo control";
+$configNextPhotoControlLabel="Label for next photo control";
+$configPreviousThumbnailsPageControlLabel="Label for previous thumbnails page control";
+$configNextThumbnailsPageControlLabel="Label for next thumbnails page control";
+$configShowLogo="Show Logo"; 
+$configLogo="Filename of the Logo"; 
+$configLogoLink="Link behind the Logo"; 
+$configShowCopyright="Show Copyright"; 
+$configCopyright="Copyright label";
+$configShowMnlfolioPromo="Show mnlfolio promotion link";
+$configShowAdminLink="Show link to the mnlfolio administration page";
+$configShowNavigationMenu="Show navigation menu";
+$configDelimiterSetsTitles="Sets titles delimiter in navigation menu";
+$configShowContact="Show contact"; 
+$configContactLabel="Contact label"; 
+$configContactEmail="Contact email address"; 
+$configShowBackgroundColorPicker="Show Background Color Picker"; 
+$configApiKey="Api key provided by Flickr"; 
+$configApiSecret="Api secret provided by Flickr"; 
+$configCacheDir="Directory where the cache must be stored"; 
+$configCacheTimeToLive="Cache Time To Live in seconds";
+$configTrue="Yes";
+$configFalse="No";
+$configMetaDescription="Specifies a description for this portfolio";
+$configMetaAuthor="Specifies an author for this porfolio";
+$configMetaKeywords="Specifies keywords for this portfolio<br/>(keywords must be comma-separated)";
+
+$configResetAll="Reset cache, Flickr link, selection of sets, the apparence, the configuration and the admin account.<br/>Great to restart from scratch";
+$configResetFlickrLink="Reset cache, Flickr link, but preserve the selection of sets, the apparence, the configuration and the admin account.<br/>Great to restart with brand new Flickr App and Token";
+
+$BASICPARAMETERS="<center>----------------------------------&nbsp;#&nbsp;<b>BASIC PARAMETERS</b>&nbsp;#&nbsp;----------------------------------</center>";
+$ADVANCEDPARAMETERS="<center>---------------------------------&nbsp;#&nbsp;<b>ADVANCED PARAMETERS</b>&nbsp;#&nbsp;--------------------------------</center>";
+$general="<b><u><i>GENERAL</i></u></b>";
+$pageParameters="<b><u><i>PAGE CUSTOMIZATION</i></u></b>";
+$internationalisation="Internationalisation";
+$pageParams="Page parameters";
+$galleryParams="Gallery";
+$guiParams="Interface";
+$controls="Navigation";
+$headerParams="Header";
+$footerParams="Footer";
+$webPromotion="Web promotion";
+$flickrParams="Flickr";
+$techParams="Technical";
+
+?>
+
---
+++ design/resources/resources-fr.php
@@ -0,0 +1,150 @@
+<?
+
+$language="Langue";
+$username="Nom d'utilisateur";
+$password="Mot de passe";
+$password2="Mot de passe &agrave; nouveau";
+$pwdQuestion="Question en cas de perte du mot de passe";
+$pwdAnswer="R&eacute;ponse";
+$lostPassword="Mot de passe oubli&eacute;?";
+
+$messageFolioNotInit="Votre compte d'administration mnlfolio n'est pas encore initialis&eacute;.<br/>Vous devez d&eacute;finir un nom d'utilisateur et un mot de passe<br/>pour pouvoir administrer de votre mnlfolio. <br/>Pour cela, merci de remplir le formulaire ci-dessous :";
+$messagePasswordsMismatch="Erreur: 'Mot de passe' et 'Mot de passe &agrave; nouveau' diff&egrave;rent";
+$messageWrongPassword="Erreur : Mauvais mot de passe, essayez &agrave; nouveau";
+$messagePasswordChanged="Votre mot de passe a &eacute;t&eacute; mis à jour";
+$messagePwdAnswerMismatch="Erreur : La r&eacute;ponse est incorrecte!";
+
+$messageInstallComplete="Installation termin&eacute;e";
+$messageInstallCompleteDescription="Votre mnlfolio est d&eacute;sormais install&eacute;. Utilisez les onglets ci-dessus pour commencer &agrave; le configurer :)<br/> <b>Si vous ne visualisez pas plusieurs onglets, appuyez sur le bouton rafraîchir de votre navigateur internet.</b>";
+
+$titleStep="Etape";
+$messageInstallStep1Hello="Salut";
+$messageInstallStep1FolioNotRegistered="Votre mnlfolio n'est pas encore enregistr&eacute; aupr&egrave;s de Flickr!";
+$messageInstallStep1NeedCompletion="Vous devez r&eacute;aliser quelques &eacute;tapes...";
+$messageInstallStep1InstructionsPart1="Enregistrer votre folio comme application aupr&egrave;s de Flickr pour qu'il puisse acc&egrave;der &agrave; vos albums Flickr.<br/><br/>Pour cela, vous avez besoin de suivre le lien ci-dessous  ('--> Enregistrez votre application aupr&egrave;s de Flickr <--') où il vous sera demand&eacute; quelques informations au sujet de ce folio.<br/><br/><b>Avant de cliquer sur le lien, veuillez lire attentivement les &eacute;tapes que vous devrez r&eacute;aliser :</b><br/><br/>1) Informer Flickr de la nature de votre folio en cliquant sur le bouton de demande de cl&eacute; associ&eacute; &agrave; la nature de votre folio (Commercial | Non Commercial). Le choix de cette nature vous appartient.<br/>2) Remplir et soumettre le formulaire de description de votre folio. Le choix des r&eacute;ponses vous appartient.<br/>3) Reporter dans le formulaire ci-dessous, la 'Cl&eacute;' et le 'Secret' qui vous seront alors communiqu&eacute;s. Ces derniers identifient votre folio aupr&egrave;s de Flickr.<br/>4) Laisser la fen&ecirc;tre Flickr ouverte, vous en aurez besoin &agrave; l'&eacute;tape 2.<br/>5) Enfin, cliquez sur 'Suivant'.<br/><br/>&nbsp;&nbsp;&nbsp;";
+$messageInstallStep1InstructionsPart2="--> Enregistrez votre application aupr&egrave;s de Flickr <--</a> (La page s'ouvrira dans une nouvelle fen&ecirc;tre/onglet).<br/>";
+$messageInstallStep1Key="Clé";
+$messageInstallStep1Secret="Secret";
+
+$messageInstallStep2InstructionsPart1="Maintenant vous avez besoin de sp&eacute;cifier un 'URL de rappel'. Pour cela, s&eacute;lectionnez et copiez le lien ci-dessous (en rouge) : ";
+$messageInstallStep2InstructionsPart2="Sur la page Flickr que vous avez laiss&eacute; ouverte, vous devez trouver en dessous de 'Secret' un lien nomm&eacute; 'Modifier le flux d’autorisation de cette application'.<br/>Si vous suivez ce lien vous devez pouvoir remplir un champ de formulaire 'URL de rappel' en collant le lien que vous venez de copier. Une fois le 'Coller' effectu&eacute;, cliquez sur le bouton 'Save changes'.";
+$messageInstallStep2InstructionsPart3="Cliquez sur le lien ci-dessous afin d'autoriser votre folio &agrave; acc&eacute;der &agrave; votre compte Flickr. Une fois que vous avez cliqu&eacute; sur 'OK, je l'autorise' vous serez reconduit sur cette page courante.";
+$messageInstallStep2InstructionsPart4="J'ai bien attribu&eacute; un URL de rappel, donc je peux d&eacute;sormais autoriser mon folio;"
+
+$messageCouldntDelete="Ne peut supprimer";
+$messageCleanCacheSucceed="Cache r&eacute;initialis&eacute; avec succ&egrave;s";
+
+$messageUploadLayout="Charger une mise en page";
+$messageUploadLogo="Charger un logo";
+$messageDeleteLayout="Supprimer une mise en page";
+$messageDeleteLogo="Supprimer un logo";
+$layout="layout";
+$logo="logo";
+
+$tabLabelMore="+";
+$tabLabelConfiguration="Configuration";
+$tabLayouts="Mises en page";
+$tabLogos="Logos";
+$tabLabelAppearance="Apparence";
+$tabLabelMySets="Mes Albums";
+$tabLabelSeeFolio="Voir Mon Folio";
+
+$unselectedSets="Albums non s&eacute;lectionn&eacute;(s)";
+$selectedSets="Albums s&eacute;lectionn&eacute;(s)";
+$defaultSet="(par d&eacute;faut)";
+
+$appearenceParams="Param&egrave;tres";
+$appearenceParamsBody="Corps";
+$appearenceParamsLinks="Liens";
+$appearenceParamsContact="Contact";
+$appearenceParamsCopyright="Copyright";
+$appearenceParamsPhotoTitle="Titre de la photo";
+$appearenceParamsPhotoDescription="Description de la photo";
+$appearenceParamsPhotoThumbFrame="Cadre de la vignette";
+$appearenceParamsPhotoThumb="Vignette";
+$appearenceParamsSelectedPhotoThumbFrame="Cadre de la vignette de la photo s&eacute;lectionn&eacute;e";
+$appearenceParamsSelectedPhotoThumb="Vignette de la photo s&eacute;lectionn&eacute;e";
+$appearenceParamsPhotoThumbMouseOver="Vignette survol&eacute;e";
+$appearenceParamsSelectedPhotoFrame="Cadre de la photo s&eacute;lectionn&eacute;e";
+$appearenceParamsSelectedPhoto="Photo s&eacute;lectionn&eacute;e";
+$appearencePreview="Aperçu";
+$appearenceBodyText="Texte de corps";
+$appearenceLinks="Liens";
+$appearenceContact="Contact";
+$appearenceCopyright="(c) copyright";
+$appearencePhotoTitle="Titre photo";
+
+$btnChooseFile="Choisissez un fichier";
+$btnUpload="Charger";
+$btnSave="Enregistrer";
+$btnConnect="Se connecter";
+$btnSaveChanges="Enregistrer les modifications";
+$btnSetAsDefault="Définir comme album par d&eacute;faut";
+$btnClearDefault="Aucun album par d&eacute;faut";
+$btnResetCache="R&eacute;initialiser le cache";
+$btnSignOut="Se D&eacute;connecter";
+$btnNext="Suivant";
+$btnRenewPwd="R&eacute;initialiser le mot de passe";
+$btnDelete="Supprimer";
+$btnResetAll="R&eacute;initialiser tout";
+$btnResetFlickrLink="R&eacute;initialiser le lien avec Flickr";
+
+$configLanguage="Langue";
+$configPageTitle="Titre de la page";
+$configShowPhotoDescription="Afficher la description des photos";
+$configShowPhotoTitle="Afficher le titre des photos";
+$configPhotoSize="Taille de la photo s&eacute;lectionn&eacute;e";
+$configGUIWidth="Largeur de la visionneuse en pixels (hors panneau vignettes)";
+$configGUIHeight="Hauteur de la visionneuse en pixels (hors panneau vignettes)";
+$configColumns="Nombre de vignettes par colonne";
+$configRows="Nombre de vignettes par ligne";
+$configLayout="Mise en page de la visionneuse";
+$configEnableKeyboardControl="Activer le contr&ocirc;le par le clavier";
+$configEnableScrollWheelControl="Activer le contr&ocirc;le par la molette de la souris";
+$configTweakSaveImageAs="Court-circuiter l'option Enregistrer l'image sous afin d'&eacute;viter les t&eacute;l&eacute;chargements de vos photos";
+$configPreviousPhotoControlLabel="Libell&eacute; pour le contr&ocirc;le photo pr&eacute;c&egrave;dente";
+$configNextPhotoControlLabel="Libell&eacute; pour le contr&ocirc;le photo suivante";
+$configPreviousThumbnailsPageControlLabel="Libell&eacute; pour le contr&ocirc;le page de vignettes pr&eacute;c&egrave;dente";
+$configNextThumbnailsPageControlLabel="Libell&eacute; pour le contr&ocirc;le page de vignettes suivante";
+$configShowLogo="Afficher le Logo"; 
+$configLogo="Nom du fichier du Logo"; 
+$configLogoLink="Lien derri&egrave;re le Logo"; 
+$configShowCopyright="Afficher le Copyright"; 
+$configCopyright="Libell&eacute; du Copyright";
+$configShowMnlfolioPromo="Affichier le lien de promotion de mnlfolio";
+$configShowAdminLink="Afficher le lien vers la page d'administration du mnlfolio";
+$configShowNavigationMenu="Afficher le menu de navigation";
+$configDelimiterSetsTitles="S&eacute;parateur de titre de sets dans le menu de navigation";
+$configShowContact="Afficher le lien Contact"; 
+$configContactLabel="Libell&eacute; du lien Contact"; 
+$configContactEmail="Adresse email du Contact"; 
+$configShowBackgroundColorPicker="Afficher le picker de couleur de fond"; 
+$configApiKey="Clé fournie par Flickr"; 
+$configApiSecret="Secret fourni par Flickr"; 
+$configCacheDir="R&eacute;pertoire où le cache doit &ecirc;tre g&eacute;n&eacute;r&eacute;"; 
+$configCacheTimeToLive="Esp&eacute;rance de vie du cache en secondes";
+$configTrue="Oui";
+$configFalse="Non";
+$configMetaDescription="Sp&eacute;cifie une description pour ce portfolio";
+$configMetaAuthor="Sp&eacute;cifie un auteur pour ce portfolio";
+$configMetaKeywords="Sp&eacute;cifie des mots cl&eacute;s pour ce portfolio<br/>(mots &agrave; s&eacute;parer par des virgules)";
+
+$configResetAll="R&eacute;initialise le cache, la connectivit&eacute; Flickr, la s&eacute;lection d'albums, l'apparence, la configuration, le compte administrateur account.<br/>Tr&egrave;s bien pour recommencer de z&eacute;ro.";
+$configResetFlickrLink="R&eacute;initialise le cache, la connectivit&eacute; Flickr, mais pr&eacute;serve la s&eacute;lection d'albums, l'apparence, la configuration, le compte administrateur account.<br/>Tr&egrave;s bien pour recommencer avec une nouvelle application Flickr et un nouveau jeton Flickr.";
+
+$BASICPARAMETERS="<center>----------------------------------&nbsp;#&nbsp;<b>PARAMETRES BASIQUES</b>&nbsp;#&nbsp;----------------------------------</center>";
+$ADVANCEDPARAMETERS="<center>-----------------------------------&nbsp;#&nbsp;<b>PARAMETRES AVANCES</b>&nbsp;#&nbsp;----------------------------------</center>";
+$general="<b><u><i>GENERAL</i></u></b>";
+$pageParameters="<b><u><i>PERSONNALISATION DE LA PAGE</i></u></b>";
+$internationalisation="Internationalisation";
+$pageParams="Page";
+$galleryParams="Galerie";
+$guiParams="Interface";
+$controls="Navigation";
+$headerParams="Ent&ecirc;te";
+$footerParams="Pied de page";
+$webPromotion="R&eacute;f&eacute;rencement web";
+$flickrParams="Flickr";
+$techParams="Techniques";
+
+?>
